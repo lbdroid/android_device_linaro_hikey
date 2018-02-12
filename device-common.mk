1116e598232a14be117261efb3db91d502fd2160
Adam Serbinski <adam@serbinski.com>
2018-02-12 12:09:06 -0500

    Remove android.hardware.broadcastradio@1.0-impl from PRODUCT_PACKAGES
    Why was it there?
    
    Change-Id: Ie2f2f965aaf12e867702a9512458d5ffdc71c12e

--- device-common.mk
+++ device-common.mk
@@ -43,7 +43,6 @@ PRODUCT_PACKAGES += audio.a2dp.default \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_PACKAGES += \
