75a8025264d5f0352584e34e972fb610e46e7294
Adam Serbinski <adam@serbinski.com>
2018-02-20 15:11:37 -0500

    Make the SWI application actually work
    It needs to be signed with platform key in order to share the system user
    
    Change-Id: Ibdf345c682227adc12744b0ae41bd8bbb8b94ef0

--- swi/Android.mk
+++ swi/Android.mk
@@ -4,6 +4,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := SWIConfig
+LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_DEX_PREOPT := false
